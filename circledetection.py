317fa296c3820de3135ba25713258422d3f8a7e2
Adam P. Szatrowski <apszatro@dhip-146.classrooms.roberts.colby.edu>
2012-05-01 13:26:07 -0400

    Implemented basic hough algorithm wth Canny edges

--- circledetection.py
+++ circledetection.py
@@ -46,11 +46,12 @@ class Grayscale(pipeline.ProcessObject):
 
 class HoughCircles(pipeline.ProcessObject):
     def __init__(self, inpt = None):
-        pipeline.ProcessObject.__init__(self, inpt)
+        pipeline.ProcessObject.__init__(self, inpt, outputCount=2)
         self.sigmaD = 1.0
-        self.max_radius = 100
+        self.max_radius = 40
         self.min_radius = 10
         self.gradient_threshold = 50.0
+        self.rad_idx = 20
 
     def generateData(self):
         orig_inpt = self.getInput(0).getData()
@@ -66,39 +67,48 @@ class HoughCircles(pipeline.ProcessObject):
         Iy = filters.gaussian_filter1d(inpt, self.sigmaD, 1, 0)
         Iy = filters.gaussian_filter1d(Iy, self.sigmaD, 0, 1)
 
-
+		
         gradMag = imgutil.normalize(numpy.sqrt(Ix*Ix+Iy*Iy).astype(numpy.uint8))
+        gradAng = numpy.arctan2(Iy,Ix)
         
         #gradMag = morphology.binary_opening(gradMag, structure=numpy.ones((3,3)), iterations=3)
         
-        output = (gradMag>self.gradient_threshold)#cv2.Canny(orig_inpt, 10, 100)
-        output = morphology.binary_dilation(output, structure=numpy.ones((3,3)), iterations=4)
-        output = morphology.binary_erosion(output, structure=numpy.ones((3,3)), iterations=4)*255
+        #output = (gradMag>self.gradient_threshold)#cv2.Canny(orig_inpt, 10, 100)
+        #output = morphology.binary_dilation(output, structure=numpy.ones((3,3)), iterations=4)
+        #output = morphology.binary_erosion(output, structure=numpy.ones((3,3)), iterations=4)
+        
+        # non-maximum supression
+        #output2 = findEdges(gradMag, gradAng, self.gradient_threshold)
+        
+        output = cv2.Canny(orig_inpt, 10, 100)
         
         #circles = cv2.HoughCircles(orig_inpt, cv.CV_HOUGH_GRADIENT,1.5, 10)[0]
         circles = []
         #if not self.max_radius:
         #    self.max_radius = max(inpt.shape[0], inpt.shape[1])
-        print gradMag.max(), gradMag.min(), gradMag.mean()
         bins = numpy.zeros((self.max_radius, gradMag.shape[0], gradMag.shape[1]), numpy.uint8)
         print output.size
+        print numpy.where(output)[0].size
         for y,x in zip(*numpy.where(output)):
             r = self.min_radius
-            r = 20
-            theta = math.atan2(Iy[y,x],Ix[y,x])
-            print theta
+            theta = gradAng[y,x]
+            #TODO: vary theta by +- .5 or so
+            sinTheta = math.sin(theta)
+            cosTheta = math.cos(theta)
             while r<self.max_radius-1:
-                bins[r, r*math.sin(theta), r*math.cos(theta)]+= 1
-                r+= self.max_radius
+            	cY, cX = y+r*sinTheta, x+r*cosTheta 
+                if cY>0 and cX>0 and cX<output.shape[1] and cY<output.shape[0]:
+                	bins[r, cY, cX]+= 1
+                r += 1
         
+        #TODO: find local maxima instead of global
+        circles = zip(*numpy.where(bins > bins.max()-20))
         
-                
-            
-        
-        for x, y, r in circles:
+        for r, y, x in circles:
             cv2.circle(inpt, (x,y), r, (0,0,255), 2)
         
-        self.getOutput(0).setData(bins[20])
+        self.getOutput(0).setData(inpt)#bins[self.rad_idx]*100)
+        self.getOutput(1).setData(output)
     
     def setSigmaD(self, sigmaD):
         self.sigmaD=sigmaD
@@ -107,23 +117,82 @@ class HoughCircles(pipeline.ProcessObject):
     def setGradientThreshold(self, gs):
         self.gradient_threshold = gs
         self.modified()
+    
+    def setRadIdx(self, idx):
+    	self.rad_idx = idx
+    	self.modified()
+
+
+def findEdges(gradMag, gradAng, magThresh=200):
+    '''
+    Finds edges from gradient magnitude and orientation images.  The orientation
+    image is converted to a coarse approximation of angle, rounded to the
+    nearest pi/4. 
+    '''
+    # make coarse version of gradient orientation--split into 4 pi/4 sections
+    piOver8 = numpy.pi / 8
+    coarseAng = numpy.copy(gradAng)
+    coarseAng[coarseAng < 0] = coarseAng[coarseAng < 0] + numpy.pi
+    coarseAng[numpy.logical_or( coarseAng < 1 * piOver8, coarseAng > 7 * piOver8)] = 0
+    coarseAng[numpy.logical_and(coarseAng > 1 * piOver8, coarseAng < 3 * piOver8)] = 2 * piOver8
+    coarseAng[numpy.logical_and(coarseAng > 3 * piOver8, coarseAng < 5 * piOver8)] = 4 * piOver8
+    coarseAng[numpy.logical_and(coarseAng > 5 * piOver8, coarseAng < 7 * piOver8)] = 6 * piOver8
+
+    # compute offsets for all pixels; 1.5 scaling ensures that diagonal 
+    # directions will round to next pixel (cos(pi/4) = sqrt(2)/2)
+    offsetRow = numpy.int8(1.5 * numpy.sin(coarseAng))
+    offsetCol = numpy.int8(1.5 * numpy.cos(coarseAng))
+        
+    # threshold gradient magnitude to exclude weak edges, and exclude boundaries
+    mag = numpy.where(gradMag >= magThresh, 1, 0) * gradMag
+    mag[0,:] = 0
+    mag[:,0] = 0
+    mag[mag.shape[0]-1,:] = 0
+    mag[:, mag.shape[1]-1] = 0
+    (rIdx, cIdx) = numpy.where(mag > 0)
+    
+    # create edge image
+    edge = numpy.zeros(mag.shape, numpy.uint8)
+    
+    # set as edges locations where gradient is high and a local maximum along
+    # the gradient direction
+    for r,c in zip(rIdx, cIdx):
+        dr, dc = offsetRow[r,c], offsetCol[r,c]
+        if mag[r, c] >= mag[r + dr, c + dc] and mag[r, c] >= mag[r - dr, c - dc]:
+            edge[r,c] = 1
+            
+    return edge
+
+
+
 
 def test_circle_detection(args):
-    pipesource = source.FileReader("/Users/danelson/Desktop/cs365final/data/2.png")
+    pipesource = source.FileReader("./data/2.png")
     grayScale = Grayscale(pipesource.getOutput())
     hCircles = HoughCircles(grayScale.getOutput())
-    display = Display(hCircles.getOutput())
+    
+    display0 = Display(pipesource.getOutput(0), "")
+    display = Display(hCircles.getOutput(0), "1")
+    
+    display2 = Display(hCircles.getOutput(1), "2")
 
     key = None
     while key != 27:
+        display0.update()
         display.update()
+        
+        display2.update()
         key = cv2.waitKey(10)
         if key == 119:
-            hCircles.setSigmaD(hCircles.sigmaD+0.02)
-            print hCircles.sigmaD
+            hCircles.setRadIdx(hCircles.rad_idx+1)
+            print hCircles.rad_idx
+            #hCircles.setSigmaD(hCircles.sigmaD+0.02)
+            #print hCircles.sigmaD
         if key == 115:
-            hCircles.setSigmaD(hCircles.sigmaD-0.02)
-            print hCircles.sigmaD
+            hCircles.setRadIdx(hCircles.rad_idx-1)
+            print hCircles.rad_idx
+            #hCircles.setSigmaD(hCircles.sigmaD-0.02)
+            #print hCircles.sigmaD
         if key == 97:
             hCircles.setGradientThreshold(hCircles.gradient_threshold+5)
             print hCircles.gradient_threshold
